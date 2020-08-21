c7af9754d999ad4df21f85efd95637aa2d5e0ffe
shanoinsano10 <shane.earley10@gmail.com>
2020-08-21 13:05:24 -0400

    Refactor to nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-navbar spaced shadow>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <span class="title is-4">Shane Earley</span>
+      </b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item>
+        <nuxt-link :to="'/'" exact-active-class="is-active">Home</nuxt-link>
+      </b-navbar-item>
+      <b-navbar-item>
+        <nuxt-link :to="'work'" exact-active-class="is-active">Work</nuxt-link>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  head() { return { title: `Shane Earley | ${this.$route.name === 'index' ? 'Home' : 'Work'}` } },
+  data() {
+    return {
+      items: [],
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+  </section>
+</template>
+
+<script>
+
+export default {
+  head() { return { title: this.$route.name } },
+  name: 'HomePage',
+  components: {
+  
+  },
+}
+</script>

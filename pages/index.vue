a876996a56929756c0ea643204ba325d75b9b33b
shanoinsano10 <shane.earley10@gmail.com>
2020-08-24 23:56:59 -0400

    Add static waves for feedback

--- pages/index.vue
+++ pages/index.vue
@@ -30,6 +30,13 @@
         </div>
       </div>
     </section>
+    <svg class="wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#226699"
+        fill-opacity="1"
+        d="M0,224L48,192C96,160,192,96,288,101.3C384,107,480,181,576,213.3C672,245,768,235,864,213.3C960,192,1056,160,1152,154.7C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+      />
+    </svg>
     <section class="section has-sapphire-blue-bg">
       <div class="container">
         <div class="tile is-ancestor">
@@ -58,6 +65,13 @@
         </div>
       </div>
     </section>
+    <svg class="wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#fff"
+        fill-opacity="1"
+        d="M0,96L48,106.7C96,117,192,139,288,154.7C384,171,480,181,576,176C672,171,768,149,864,149.3C960,149,1056,171,1152,176C1248,181,1344,171,1392,165.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+      />
+    </svg>
     <section class="section">
       <div class="container">
         <div class="tile is-ancestor is-reversed-mobile has-text-centered">
@@ -83,8 +97,8 @@
                       class="button is-blue-jeans is-medium footer-button"
                       @click="cardModal()"
                     >
-                    <b-icon pack="fas" icon="envelope"  />
-                    <span>Send a message</span>
+                      <b-icon pack="fas" icon="envelope" />
+                      <span>Send a message</span>
                     </button>
                     <a
                       class="button is-sapphire-blue is-medium footer-button"
@@ -92,8 +106,8 @@
                       rel="noopener noreferrer"
                       href="/Shane_Earley_Resume_August_2020.pdf"
                     >
-                    <b-icon pack="fas" icon="external-link-alt"  />
-                    <span>View my resume</span>
+                      <b-icon pack="fas" icon="external-link-alt" />
+                      <span>View my resume</span>
                     </a>
                   </div>
                 </div>
@@ -114,6 +128,13 @@
         </div>
       </div>
     </section>
+    <svg class="wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path
+        fill="#AEBBC1"
+        fill-opacity="1"
+        d="M0,32L48,64C96,96,192,160,288,176C384,192,480,160,576,160C672,160,768,192,864,224C960,256,1056,288,1152,293.3C1248,299,1344,277,1392,266.7L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+      />
+    </svg>
   </div>
 </template>
 
@@ -128,9 +149,7 @@ export default {
     Form,
   },
   data() {
-    return {
-
-    }
+    return {}
   },
   methods: {
     cardModal() {
@@ -196,5 +215,17 @@ export default {
 }
 .footer-button {
   margin: 0.5em;
+}
+.wave-svg {
+  display: block;
+}
+.wave-svg:nth-of-type(1) {
+  background: #55acee;
+}
+.wave-svg:nth-of-type(2) {
+  background: #226699;
+}
+.wave-svg:nth-of-type(3) {
+  background: #fff;
 }
 </style>
